011190aea90674c8d156ca1460b370e74529a093
Long Pham <9040472+longmpham@users.noreply.github.com>
2023-05-05 17:12:43 -0700

    added getting comments and it works very nicely.

--- get_screenshot.py
+++ get_screenshot.py
@@ -1,10 +1,16 @@
 import os
+import requests
+import random
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def take_screenshot(url):
+used_numbers = []
+
+
+def get_post_screenshot(url):
     # Start a new Chrome browser instance
     browser = webdriver.Chrome()
 
@@ -13,7 +19,8 @@ def take_screenshot(url):
     browser.maximize_window()
 
     # Wait for the element to be visible
-    post_element = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="t3_137ua7v"]')))
+    post_element = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="t3_137ua7v"]')))
 
     # Take a screenshot of the element
     screenshot = post_element.screenshot_as_png
@@ -22,7 +29,7 @@ def take_screenshot(url):
     screenshot_folder = os.path.join(os.getcwd(), 'resources', 'screenshots')
     if not os.path.exists(screenshot_folder):
         os.makedirs(screenshot_folder)
-    screenshot_path = os.path.join(screenshot_folder, 'screenshot.png')
+    screenshot_path = os.path.join(screenshot_folder, 'screenshot_post.png')
     with open(screenshot_path, 'wb') as f:
         f.write(screenshot)
 
@@ -30,3 +37,65 @@ def take_screenshot(url):
     browser.quit()
 
     return screenshot_path
+
+
+def get_comment_screenshot(url, max_num_of_comments=3):
+
+    def get_new_comment_id():
+        global used_numbers
+        new_comment_id = random.randint(3, 10)
+        while new_comment_id in used_numbers:
+            new_comment_id = random.randint(3, 10)
+        used_numbers.append(new_comment_id)
+        return new_comment_id
+
+    url_json = url + ".json"
+    max_num_of_comments = max_num_of_comments
+    comment_id = 2  # start at 2, the 0,1 are bots.
+    # get the url data from the url given
+    response = requests.get(url_json, headers={'User-agent': 'Mozilla/5.0'})
+    data = response.json()
+    comments_data = data[1]['data']['children']
+    # print(comments_data)
+
+    # Start a new Chrome browser instance
+    browser = webdriver.Chrome()
+
+    # Navigate to the URL
+    browser.get(url)
+    browser.maximize_window()
+
+    # Scroll down by 1000 pixels
+    browser.execute_script("window.scrollBy(0, 2000)")
+    time.sleep(3)
+
+    for i in range(max_num_of_comments):
+        # Get a comment number and the comment post
+        comment_id += 1  # for the top n comments
+        # comment_id = random.randint(3, 10) # for random comments
+        # comment_id = get_new_comment_id()
+        post_element = WebDriverWait(browser, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, f'//*[@id="comment-tree"]/shreddit-comment[{comment_id}]')))
+
+        # Take a screenshot of the element
+        screenshot = post_element.screenshot_as_png
+
+        # Save the screenshot to a file
+        screenshot_folder = os.path.join(
+            os.getcwd(), 'resources', 'screenshots')
+        if not os.path.exists(screenshot_folder):
+            os.makedirs(screenshot_folder)
+        screenshot_path = os.path.join(
+            screenshot_folder, f'screenshot_comment_{i+1}.png')
+        with open(screenshot_path, 'wb') as f:
+            f.write(screenshot)
+
+        time.sleep(1)
+
+    # Close the browser
+    browser.quit()
+    return screenshot_path
+
+
+get_comment_screenshot(
+    "https://www.reddit.com/r/AmItheAsshole/comments/138csc5/wibta_for_spraying_some_kid_with_my_garden_hose/", 3)
