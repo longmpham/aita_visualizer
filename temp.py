7ea53d321a1970d394ac8b426f0d347b3d80121b
Long Pham <9040472+longmpham@users.noreply.github.com>
2023-05-07 16:50:07 -0700

    fixed weird bugs..clean start.

--- temp.py
+++ temp.py
@@ -11,8 +11,6 @@ from gtts import gTTS
 # from youtube_uploader_selenium import YouTubeUploader
 # from selenium.webdriver.common.by import By
 
-from get_screenshot import get_full_screenshot
-
 acronyms_dict = {
     "AITA": "Am I the A-Hole",
     "BTW": "By the Way",
@@ -178,6 +176,7 @@ def format_meta_text(post):
     save_json(post_meta_json, file_name="yt_meta_data.json")
     return post_meta
 
+
 def save_json(post, file_name="post.json"):
     file_name = file_name
     with open(file_name, "w") as outfile:
@@ -252,24 +251,6 @@ def format_text(post):
     return post_formatted
 
 def createClip(post, mp3_file="post-text.mp3"):
-
-    def get_video_file():
-        background_video_dir = os.path.join(os.getcwd(), "resources", "background_videos")
-        background_video_files  = [f for f in os.listdir(background_video_dir) if f.endswith(".mp4")]
-        background_video_file = os.path.join(background_video_dir, random.choice(background_video_files))
-        return background_video_file
-    
-    def get_screenshot_file():
-
-        screenshots = get_full_screenshot()
-        
-        # # screenshots = []
-        # screenshots_dir = os.path.join(os.getcwd(), "resources", "screenshots")
-        # # screenshots = [f for f in os.listdir(screenshots_dir) if f.endswith(".png")]
-        # screenshots.append(os.path.join(screenshots_dir, "screenshot_title.png"))
-        # screenshots.append(os.path.join(screenshots_dir, "screenshot_body.png"))
-        # screenshots.append([os.path.join(screenshots_dir, f) for f in os.listdir(screenshots_dir) if "comment" in f])
-        return screenshots
 
     def create_post_text_for_video(post, audio_duration, post_meta, meta_duration):
         total_words = len(post.split())
@@ -350,8 +331,11 @@ def createClip(post, mp3_file="post-text.mp3"):
     post = format_text_json(post)
     post_full = format_text(post)
     post_meta = format_meta_text(post)
-    screenshot_files = get_screenshot_file()
-    background_video_file = get_video_file()
+    screenshot_file = ["screenshot.png"] # in a list since we're just using still images.
+    background_video_dir = ".\\resources\\background_videos"
+    background_video_files  = [f for f in os.listdir(background_video_dir) if f.endswith(".mp4")]
+    background_video_file = os.path.join(background_video_dir, random.choice(background_video_files))
+    # video_files = os.getcwd() + f"\\resources\\background_videos\\{background_video_file}"
     mp4_file = "Top AITA of the Day.mp4"
     width = 720
     height = int(width*9/16) # 16/9 screen
@@ -372,9 +356,6 @@ def createClip(post, mp3_file="post-text.mp3"):
     audio = audio.set_start(meta_duration)
     print("Audio Set...")
 
-    # Intro Clip
-    # intro_video = ImageClip(screenshot_files[0], duration=meta_duration)
-
     # Set up the video clip from our screenshot or videos
     # video = ImageSequenceClip(screenshot_file, fps=1)
     video = VideoFileClip(background_video_file).loop(duration=10)
@@ -387,6 +368,8 @@ def createClip(post, mp3_file="post-text.mp3"):
     text_clips = create_post_text_for_video(post_full, audio.duration, post_meta, meta_duration)
     # text_clips = create_post_text_for_video_scroll(post_full, audio.duration)
     print("Texts Generating...")
+
+    # Bind the audio and the video together
 
     # Bind the audio/video to the textclips
     final_clip = CompositeVideoClip([background_clip, *text_clips], size=mobile_video_size)
